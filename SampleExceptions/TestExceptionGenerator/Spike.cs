d8ee6a61064942fdede2e56ae00b8d17eab7d243
Jason Jarrett <jason@elegantcode.com>
2013-12-07 22:24:59 -0800

    adding a sample dot net exception generation project

---
+++ SampleExceptions/TestExceptionGenerator/Spike.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using Xunit;
+namespace TestExceptionGenerator
+{
+
+	public class Spike
+	{
+
+		[Fact]
+		public void GenericException()
+		{
+			Action action = () => { GetException(); };
+			var x = action.GetExceptionString();
+		}
+
+
+		[Fact]
+		public void GenericExceptionWithInnerException()
+		{
+			Action action = () => { GetInnerException(); };
+			var x = action.GetExceptionString();
+		}
+
+
+
+		public void GetException()
+		{
+			throw new Exception("Hello Exception!");
+		}
+
+		public void GetInnerException()
+		{
+			try
+			{
+				GetException();
+			}
+			catch (Exception ex)
+			{
+
+				throw new Exception("Test outer exception", ex);
+			}
+
+			
+		}
+
+	}
+
+
+	public static class Extensions
+	{
+		public static string GetExceptionString(this Action action)
+		{
+			try
+			{
+				action();
+			}
+			catch (Exception ex)
+			{
+				return ex.ToString();
+			}
+
+			throw new Exception("Expected an exception, but there wasn't any");
+		}
+
+	}
+}
